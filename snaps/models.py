c1ca837cfa10f0edbd98ef4e3a9a991dd62291ca
t3chn0tr0n <technotron.avik@gmail.com>
2018-07-29 22:01:33 +0530

    add model for the snaps app

--- snaps/models.py
+++ snaps/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Snap(models.Model):
+    image = models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField()
+    caption = models.CharField(max_length=20)
+    likes = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def __str__(self):
+        return self.caption
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
