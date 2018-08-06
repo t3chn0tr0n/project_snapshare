bb533878b3ceacb91ad4529cdc860c249a26f63d
t3chn0tr0n <technotron.avik@gmail.com>
2018-08-06 22:01:06 +0530

    fix cross-login/signup bug - previously login/signup page was available even though a user is loged in - now accessing the login/signup page redirects to last page or index

--- accounts/views.py
+++ accounts/views.py
@@ -1,34 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.http import HttpResponseRedirect
 
 
 def signup(request):
-    if request.method == 'POST':
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.get(username=request.POST['username'])
-                return render(request, 'accounts/signup.html', {'error':'Username has already been taken'})
-            except User.DoesNotExist:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-                auth.login(request,user)
-                return redirect('index')
-        else:
-            return render(request, 'accounts/signup.html', {'title':'signup', 'error':'Passwords must match'})
+    if User.is_authenticated:
+        return redirect(request.META.get('HTTP_REFERER', 'index'))
     else:
-        return render(request, 'accounts/signup.html', {'title':'signup'})
+        if request.method == 'POST':
+            if request.POST['password1'] == request.POST['password2']:
+                try:
+                    user = User.objects.get(username=request.POST['username'])
+                    return render(request, 'accounts/signup.html', {'error':'Username has already been taken'})
+                except User.DoesNotExist:
+                    user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                    auth.login(request,user)
+                    return redirect('index')
+            else:
+                return render(request, 'accounts/signup.html', {'title':'signup', 'error':'Passwords must match'})
+        else:
+            return render(request, 'accounts/signup.html', {'title':'signup'})
 
 
 def login(request):
-    if request.method == 'POST':
-        user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
-        if user is not None:
-            auth.login(request, user)
-            return redirect('index')
-        else:
-            return render(request, 'accounts/login.html',{"title":"log in", 'error':'username or password is incorrect.'})
+    if User.is_authenticated:
+        return redirect(request.META.get('HTTP_REFERER', 'index'))
     else:
-        return render(request, 'accounts/login.html', {"title":"log in"})
+        if request.method == 'POST':
+            user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+            if user is not None:
+                auth.login(request, user)
+                return redirect('index')
+            else:
+                return render(request, 'accounts/login.html',{"title":"log in", 'error':'username or password is incorrect.'})
+        else:
+            return render(request, 'accounts/login.html', {"title":"log in"})
 
 
 def  logout(request):
